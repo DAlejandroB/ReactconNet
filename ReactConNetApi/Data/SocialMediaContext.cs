e00bfaf07888e62c4864c25184e668b3e9879fa6
DAlejandroB <diegoaenciso76@gmail.com>
2024-11-15 20:20:01 -0500

    [Add] Setup dbContext

---
+++ ReactConNetApi/Data/SocialMediaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactConNetApi.Models;
+
+namespace ReactConNetApi.Data
+{
+    public class SocialMediaContext : DbContext
+    {
+        public SocialMediaContext(DbContextOptions<SocialMediaContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
